51f8e186b5168476e0977cd6122648706382edf6
Alexandre D'Astous <po09i@hotmail.com>
2023-08-14 19:03:04 -0400

    Update to follow dcm2bids 3.0

--- batch_dicom_to_nifti.sh
+++ batch_dicom_to_nifti.sh
@@ -4,7 +4,9 @@
 #
 # The script takes in an argument on the command line to where a folder of folders of DICOMs is located. The script
 # will run st_dicom_to_nifti on each folder inside the folder of the first argument and use their name to differentiate
-# them in the output. It will output in the output folder (2nd argument) in BIDS format.
+# them in the output. Since the BIDS specification requires alphanumerical labels, non alphanumerical characters
+# will be stripped from the folder names when converting to participant labels. It will output in the output folder
+# (2nd argument) in BIDS format.
 #
 # Example:
 # sh batch_dicom_to_nifti.sh input_folder output_folder
@@ -12,8 +14,8 @@
 #
 # Input folder structure:
 # input_folder/
-# input_folder/subject1/
-# input_folder/subject2/
+# input_folder/subject_1/
+# input_folder/subject_2/
 #
 # Output folder structure
 # output_folder/bids_folder/sub-subject1/...
@@ -27,20 +29,24 @@
 # correctly parse your data. More information can be found here: https://unfmontreal.github.io/Dcm2Bids/docs/how-to/create-config-file/
 
 # Create the absolute path out of the input provided to the script
-INPUT_PATH="$(cd "$(dirname "$1")" && pwd)/$(basename "$1")"
+INPUT_PATH="$( cd -- "${1}" >/dev/null 2>&1 ; pwd -P )"
+echo "Input path: ${INPUT_PATH}"
 
 # Create the absolute path out of the input provided to the script
-OUTPUT_PATH="$(cd "$(dirname "$2")" && pwd)/$(basename "$2")"
+OUTPUT_PATH="$( cd -- "${2}" >/dev/null 2>&1 ; pwd -P )"
+echo "Output path: ${OUTPUT_PATH}"
+echo
 
 # Run st_dicom_to_nifti on each folder
 for FNAME in "${INPUT_PATH}/"*; do
   if [[ -d "${FNAME}" && ! -L "${FNAME}" ]]; then
 
     echo "Processing: ${FNAME}"
-    BASENAME="$(basename "${FNAME}")"
+
+    BASENAME="$(echo "$(basename "${FNAME}")" | tr -cd '[a-zA-Z0-9]')"
     st_dicom_to_nifti -i "${FNAME}" -o "${OUTPUT_PATH}/bids_folder" --subject "${BASENAME}" || exit
 
   fi
 done
 
-echo -e "\n\033[0;32mOutput is located here: ${OUTPUT_PATH}/bids_folder"
+echo -e "\n\033[0;32mOutput is located in: ${OUTPUT_PATH}/bids_folder"
